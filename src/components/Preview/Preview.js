d8c16cd9990e14afd8e4985a89ea92efa2d5b826
nekusu <nekusu.dev@gmail.com>
2022-05-28 07:09:47 -0300

    Add Work Experience preview

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Experience from "./components/Experience";
 import '../../styles/Preview.css';
 
 function Preview({ data }) {
@@ -8,16 +9,26 @@ function Preview({ data }) {
     contact,
     education,
     skills,
+    work,
   } = data;
 
   return (
-    <div className="Preview">
-      <Header info={info} />
-      <Sidebar
-        contact={contact}
-        education={education}
-        skills={skills}
-      />
+    <div className="PreviewContainer">
+      <div className="Preview">
+        <Header info={info} />
+        <Sidebar
+          contact={contact}
+          education={education}
+          skills={skills}
+        />
+        <main>
+          <section className="Profile">
+            <h3>PROFILE</h3>
+            <p>{info.profile}</p>
+          </section>
+          <Experience name="Work Experience" work={work} />
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Preview/components/Sidebar.js
+++ src/components/Preview/components/Sidebar.js
@@ -1,12 +1,12 @@
 import Contact from "./Contact";
-import Education from "./Education";
+import Experience from "./Experience";
 import Skills from "./Skills";
 
 function Sidebar({ contact, education, skills }) {
   return (
     <div className="Sidebar">
       <Contact contact={contact} />
-      <Education education={education} />
+      <Experience name="Education" education={education} />
       <Skills skills={skills} />
     </div>
   );
