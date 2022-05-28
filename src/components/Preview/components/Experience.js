67f2937a08a477ab2970e5775714667b62552c6a
nekusu <nekusu.dev@gmail.com>
2022-05-28 07:09:37 -0300

    Create Experience component

---
+++ src/components/Preview/components/Experience.js
@@ -0,0 +1,34 @@
+import ExperienceItem from "./ExperienceItem";
+
+function Experience(props) {
+  const { name } = props;
+  const propertyName = name.toLowerCase().split(' ')[0];
+  console.log(propertyName);
+
+  return (
+    <section className={name.replace(/\s/g, '')}>
+      <h3>{name.toUpperCase()}</h3>
+      {props[propertyName].map(({
+        degree,
+        title,
+        university,
+        company,
+        from,
+        to,
+        description,
+        key,
+      }) => (
+        <ExperienceItem
+          key={key}
+          title={degree || title}
+          name={university || company}
+          from={from}
+          to={to}
+          description={description}
+        />
+      ))}
+    </section>
+  );
+}
+
+export default Experience;
